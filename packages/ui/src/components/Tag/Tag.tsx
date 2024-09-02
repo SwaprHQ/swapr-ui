1ccdb2f3ef90eea1ae500cc8e17d8aa32b3b658d
ElRodrigote <lil.rorry@gmail.com>
2024-09-02 13:50:03 +0200

    fix: consume the extended twMerge fn in our Tag component

--- packages/ui/src/components/Tag/Tag.tsx
+++ packages/ui/src/components/Tag/Tag.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { cva } from "class-variance-authority";
-import { twMerge } from "tailwind-merge";
+import { twMerge } from "../../extended-tw-merge";
 
 export const tagStyles = cva(
   ["flex items-center justify-center", "rounded-6 font-semibold border"],
