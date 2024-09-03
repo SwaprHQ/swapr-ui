109d1d2294e209638aa40d05c56faf014b9c249a
ElRodrigote <lil.rorry@gmail.com>
2024-09-03 20:07:48 +0200

    feat: update UI storybook config to resolve TS aliases

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -51,6 +53,17 @@ const config: StorybookConfig = {
       },
     },
   }),
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ];
+    }
+    return config;
+  },
 };
 
 export default config;
