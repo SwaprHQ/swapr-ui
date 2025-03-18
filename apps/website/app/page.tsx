305444701162f16c2282d5c2e8d7e5878a85f7bf
Diogo Ferreira <diogomf@hey.com>
2025-03-18 17:50:03 +0000

    update swapr-ui website

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
     <div className="bg-[url('/lines.svg')] h-screen bg-no-repeat bg-cover flex justify-center">
       <div className="pt-48 space-y-6 text-center">
         <h1 className="text-[58px] leading-[64px] font-bold">
-          Build dapps fast
+          Build apps fast
         </h1>
         <p className="text-lg text-text-med-em">
-          A set of components made to help you ship web3 apps faster than ever.
+          A set of components made to help you ship apps faster than ever.
         </p>
         <Link href="/ui" className="block">
           <Button onClick={() => console.log("hey")} className="mx-auto">
--- apps/website/app/ui/page.tsx
+++ apps/website/app/ui/page.tsx
@@ -26,9 +26,15 @@ export default function UI() {
     <main className="max-w-screen-xl px-5 mx-auto my-10 overflow-auto">
       <ThemeSwitch />
       <div className="pb-12 my-12 space-y-12 divide-y divide-surface-surface-smoke-gray-em">
-        <div className="space-y-4">
+        <div className="p-10 space-y-4 bg-surface-primary-base-em rounded-12 shadow-focus-primary">
           <h1 className="text-[42px] font-bold mb-8">Swapr UI</h1>
-          <div className="max-w-2xl space-y-2">
+          <div className="flex">
+            <div className="bg-surface-primary-high-em size-3"></div>
+            <div className="bg-surface-success-med-em size-3"></div>
+            <div className="bg-surface-danger-med-em size-3"></div>
+            <div className="bg-surface-info-med-em size-3"></div>
+          </div>
+          <div className="max-w-2xl space-y-2 text-text-med-em">
             <p>
               Create create awesome user interfaces without the hussle of
               rebuild the same component all over again.
@@ -38,10 +44,49 @@ export default function UI() {
               <a
                 href="https://atomizedesign.com/"
                 className=" text-text-primary-med-em hover:text-text-primary-high-em"
+                target="_blank"
               >
                 Atomize Design System
+              </a>{" "}
+              built by{" "}
+              <a
+                href="https://swaprhq.io/"
+                className=" text-text-primary-med-em hover:text-text-primary-high-em"
+                target="_blank"
+              >
+                Swapr
               </a>
             </p>
+            <p>
+              See it in action on{" "}
+              <a
+                className="relative text-text-low-em hover:text-text-high-em"
+                href="https://presagio.pages.dev/"
+                target="_blank"
+              >
+                👁️ Presagio{" "}
+                <div className="absolute top-0 -right-2 size-1.5 rounded-100 bg-surface-success-high-em"></div>
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-between w-full">
+            <div></div>
+            <div className="flex items-center space-x-2">
+              <a
+                className="hover:underline hover:text-text-primary-high-em"
+                href="https://github.com/SwaprHQ/swapr-ui"
+                target="_blank"
+              >
+                Github
+              </a>
+              <a
+                className="hover:underline hover:text-text-primary-high-em"
+                href="https://www.npmjs.com/package/@swapr/ui"
+                target="_blank"
+              >
+                NPM
+              </a>
+            </div>
           </div>
         </div>
         <ButtonSection />
